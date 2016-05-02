fb67457b883712856d2dd6339bccc25163d13c3c
LegitSoulja <macballerboy@gmail.com>
2016-05-01 18:48:28 -0600

    Update cmd.js

--- test/cmd.js
+++ test/cmd.js
@@ -1,29 +1,40 @@
+/* global socket */
+
 'use strict';
 
 // OgarConsole Settings
 function OgarConsoleSettings(){
 	
-    // OgarConsole Port  
-    this.serverPort = 1000;
+	// OgarConsole Port
+	this.serverPort = 1000;
 	
-    // Advanced Mode (NOT RECOMMENDED TO BE TRUE)
-    this.advanced = false;
+        // OgarConsole password protection
+        this.requirePassword = true;
+        
+        // OgarConsole password apon connecting
+        this.consolePassword = "OgarConsole123";
+        
+        // Allow OgarConsole origin to bypass login approvals.
+        this.allowOriginPasswordBypass = true;
+        
+	// Advanced Mode (NOT RECOMMENDED TO BE TRUE)
+	this.advanced = false;
 	
-    // Enable this to allow the server to be terminated. False make this server non stoppable.
-    this.allowExit = false;
+	// Enable this to allow the server to be terminated. False make this server non stoppable.
+	this.allowExit = false;
 
-    // Console Log > Set 'ServerLogLevel = 1' in gamesettings.ini, Else, You will get OgarConsole errors.
-    // If file error replace with \cmd.php or /cmp.php.
-    this.log = "./logs/console.log";
+	// Console Log > Set 'ServerLogLevel = 1' in gamesettings.ini, Else, You will get OgarConsole errors.
+	// If file error replace with \cmd.php or /cmp.php.
+	this.log = "./logs/console.log";
 	
-    // OgarConsole PHP File. If file error replace with \cmd.php or /cmp.php
-    this.consoleFile = "\\cmd.php";
+	// OgarConsole PHP File. If file error replace with \cmd.php or /cmp.php
+	this.consoleFile = "\\cmd.php";
 	
-    // OgarConsole package.json. **REQUIRED**
-    this.json = require("./package.json");
+	// OgarConsole package.json. **REQUIRED**
+	this.json = require("./package.json");
 	
-    // OgarConsole Version
-    this.version = this.json.version;
+	// OgarConsole Version
+	this.version = this.json.version;
 	
 }
 
@@ -33,13 +44,35 @@ function OgarConsoleSettings(){
 
 var ogar = require('./index'),
 settings = new OgarConsoleSettings(),
-gameServer = ogar.gameServer,
+gameServer = ogar.gameServer.gameServer,
 express = require("express"),
 app = express(),
 fs = require("fs"),
 server = require('http').createServer(app),
 io = require("socket.io").listen(server),
 exec = require('child_process').exec;
+
+var newUserPassword = function(){
+    
+    this._password = "";
+    
+};
+
+newUserPassword.prototype.setPassword = function(password){
+    
+    this._password = password;
+    
+};
+
+if(settings.consolePassword.length < 3){
+    
+    console.log("[OgarConsole] Password must be more than 3 length");
+    
+    gameServer.socketServer.close();
+    process.exit(1);
+    return;
+    
+}
 
 //
 // DO NOT EDIT ABOVE
@@ -53,47 +86,52 @@ app.set("title", "OgarConsole > " + settings.version);
 
 // OgarConsole Server Start Error
 server.on('error', function(err){
-    
+	
     console.log("[OgarConsole] Could not listen on port " + settings.serverPort + ". Try using a different port.");
     console.log("[Console] Ogar and OgarConsole stopped..");
     gameServer.socketServer.close();
     process.exit(1);
     return;
-    
+	
 });
 
 // OgarConsole Listen For Connections.
 app.get("/", function(req, res) {
-    
     fs.readFile(__dirname + settings.consoleFile, function(err, data) {
-        
         if(!err){
-            
             res.send("" + data);
-            
         }else{
-            
             res.send("" + err);
             socket.emit("input", err.toString());
-            
         }
-        
     });
-    
 });
 
 // OgarConsole Socket Connection.
 io.sockets.on("connection", function(socket) {
-	
+    
     var host = socket.handshake.headers.host.split(':');
-	
+    var login = new newUserPassword();
+    
     socket.on("commandex", function(data) {
-		
-        if(host[0] != "ogar.ml" && host[0] != "localhost"){
-			
-            socket.emit("input", "Origin Disabled >> " + host[0] + ". Please visit http://ogar.ml");
+        
+        if(host[0] !== "ogar.ml" && host[0] !== "localhost"){
+            
+            socket.emit("input", "Origin Disabled >> " + host[0] + ". Please visit http://ogar.ml to connect to this console");
             return;
-			
+            
+        }else{
+            
+            if(settings.allowOriginPasswordBypass){
+
+                if(login._password.length < 3){
+                    
+                    login._password = settings.consolePassword;
+                    
+                }
+
+            }
+            
         }
 		
         if (!settings.advanced) {
@@ -108,46 +146,120 @@ io.sockets.on("connection", function(socket) {
 				
                 var split = data.split(" ");
                 var first = split[0].toLowerCase();
-                var execute = gameServer.commands[first];
+                
+                if(first !== "-password" && settings.requirePassword){
+                    
+                    if(login._password){
+
+                        if(login._password === settings.consolePassword){
+                            
+
+                        }else{
+
+                            socket.emit("input", "Invalid password. Please enter correct password!");
+                            return;
+
+                        }
+
+                    }else{
+
+                        socket.emit("input", "Please enter password. Usage: '-password password'");
+                        return;
+
+                    }
+                    
+                }
+                
 				
                 switch(first){
 					
 					
                     // Ifyou want to disable some incoming command. Maybe the Ogar verison your using has a 
-                    // command that you dont't want allowed you can redirect them here. 	
+                    // command that you dont't want allowed you can redirect them here. 
                     // Add any command you want to block || do other things.. Experience users only.
-					
-                    case "clr":
-                        fs.truncate(settings.log, "", function(){})
-                        return;
-					
-                    case "clear":
-                        fs.truncate(settings.log, "", function(){})
-                        return;
-					
-                    case "exit":	
-                        if(!settings.allowExit){
-                            socket.emit("input", "You are not allowed to terminate this server!");
+				
+                    // -password command. : usage: '-password password'
+                    case "-password":
+                        
+                        if(settings.requirePassword){
+                            
+                            if(split.length === 2){
+
+                                if(split[1] === settings.consolePassword){
+
+                                    if(login._password.length < 3){
+
+                                        login.setPassword(split[1]);
+                                        socket.emit("input", "Logged In.&#10;&#10;Type 'help' for commands.");
+
+                                        console.log("[OgarConsole] User Logged In :: " + socket.handshake.address);
+
+                                        return;
+
+                                    }else{
+
+                                        socket.emit("input", "You are already logged into the console.&#10;&#10;Type 'help' for commands.");
+                                        return;
+
+                                    }
+
+                                }else{
+
+                                    socket.emit("input", "Invalid password. Please try again");
+                                    return;
+
+                                }
+
+                            }else{
+
+                                socket.emit("input", "Usage: '-password password'");
+                                return;
+
+                            }
+                        }else{
+                            
+                            socket.emit("input", "This console is not password protected!.");
                             return;
-							
-                        }		
+                            
+                        }
                         break;
+                        
+                    case "logout":
+                        login._password = "";
+                        socket.emit("input", "You have been logged out. Please login.");
+                        return;
+                    
+                    case "clr":
+                        fs.truncate(settings.log, "", function(){});
+                        return;
+                        
+                    case "clear":
+                        fs.truncate(settings.log, "", function(){});
+                        return;
 					
-                    case "stop":	
+                    case "exit":		
+                        if(!settings.allowExit){
+                            socket.emit("input", "You are not allowed to terminate this server!");
+                        return;				
+                    }
+						
+                    break;
+					
+                    case "stop":		
                         if(!settings.allowExit){
                             socket.emit("input", "You are not allowed to terminate this server!");
                             return;
-							
-                        }		
+                        }
                         break;
 					
                     case "start":
                         return;
-					
-					
+							
                 }
-				
-                if (typeof execute != 'undefined') {
+
+                var execute = gameServer.consoleService.commands[first];
+                
+                if (typeof execute !== 'undefined') {
 					
                     execute(gameServer, split);
 					
@@ -164,7 +276,7 @@ io.sockets.on("connection", function(socket) {
                             socket.emit("input", err.toString());
 							
                         }
-						
+                        
                     });
 					
                 } else {
@@ -179,18 +291,19 @@ io.sockets.on("connection", function(socket) {
 				
             }
         }else{
-			
+            
             // Advanced Mode.. Executes pure cmd commands, instead of Ogar game commands.
             // This is very dangerous to have enabled, this could leave serious damage to your server
             // If you don't know what you are doing. Please keep advanced mode false, unless you know what you
             // Are doing!.
 			
-            exec(data, function(e,s,t){		
-                
+            exec(data, function(e,s,t){
+				
                 socket.emit("input", s);
                 return;
-		
+				
             });
+			
         }
         
     });
@@ -198,9 +311,7 @@ io.sockets.on("connection", function(socket) {
 });
 
 // Set CMD Title 
-exec("title OgarConsole " + settings.version + "/ Port " + settings.serverPort, function(e, s, t) {})
-
-
+exec("title OgarConsole " + settings.version + "/ Port " + settings.serverPort, function(e, s, t) {});
 
 //
 // END OF FILE
